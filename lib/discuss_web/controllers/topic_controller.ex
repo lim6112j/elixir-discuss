cf6c75c976d062400a9a34db2f3c27d48d59c4df
lim6112j <lim6112j@gmail.com>
2023-04-21 02:58:22 +0900

    more commit

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -2,15 +2,23 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
   alias Discuss.Topic
   alias Discuss.Repo
+
   def new(conn, _params) do
     struct = %Topic{}
     params = %{}
     changeset = Discuss.Topic.changeset(struct, params)
-    render conn, "new.html", changeset: changeset
+    render(conn, "new.html", changeset: changeset)
   end
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render(conn, "index.html", topics: topics)
+  end
+
   def create(_conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
     IO.inspect(changeset, label: "Inspecting.................")
+
     case Repo.insert(changeset) do
       {:ok, post} -> IO.inspect(post, label: "POST ..............")
       {:error, changeset} -> IO.inspect(changeset, label: "Changeset ...........")
