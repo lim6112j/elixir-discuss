8e5f381e8ffbd31930816e77a20c1aa104420635
ben lim <lim6112j@gmail.com>
2023-04-24 16:31:03 +0900

    edit route

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -15,13 +15,35 @@ defmodule DiscussWeb.TopicController do
     render(conn, "new.html", changeset: changeset)
   end
 
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+    render(conn, "edit.html", changeset: changeset, topic: topic)
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    changeset = Repo.get(Topic, topic_id) |> Topic.changeset(topic)
+
+    case Repo.update(changeset) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "task modified")
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, _} ->
+        render(conn, "edit.html", changeset: changeset)
+    end
+  end
+
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
     IO.inspect(changeset, label: "Inspecting.................")
 
     case Repo.insert(changeset) do
-      {:ok, post} ->
-        IO.inspect(post, label: "POST ..............")
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "created task")
+        |> redirect(to: "/")
 
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
